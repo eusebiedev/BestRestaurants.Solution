8eebeef5958481e53d1b2f54651f4bf7dfca8408
Eusebie Siebenberg <siebenee@gmail.com>
2023-05-17 16:29:16 -0700

    Add project file directory
    Co-Authored by: Joe Wilfong

---
+++ BestRestaurants/Models/Cuisine.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+
+namespace BestRestaurants.Models
+{
+  public class Cuisine
+  {
+    public int CuisineId { get; set; }
+    public string Name { get; set; }
+    public string Type { get; set; }
+    public List<Restaurant> Restaurants { get; set; }
+  }
+}
